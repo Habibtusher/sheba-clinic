863221b965185e6f5425f42365d1511c9cd7352a
Habibtusher <tusherhrt@gmail.com>
2023-03-05 21:28:24 +0600

    interceptors update

--- src/api/Interceptors.js
+++ src/api/Interceptors.js
@@ -4,7 +4,7 @@ import {base_url } from "./BaseUrl";
 export const API = axios.create({baseURL:base_url})
 
 API.interceptors.request.use((req)=>{
-const token =localStorage.getItem("access_token")
+
     if(localStorage.getItem("access_token")){
 
         req.headers.authorization= `bearer ${localStorage.getItem("access_token")}` 
